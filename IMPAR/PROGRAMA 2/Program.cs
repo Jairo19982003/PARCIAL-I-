d00022239ce1b74692a84d2b6adf6a98dec08117
Jairo <Jairo>
2023-03-03 19:30:57 -0600

    PRIMER PARCIAL PROGRAMACION I

---
+++ IMPAR/PROGRAMA 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Parcial_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Favor de ingresar un numero entero positivo mayor que cero. ");
+            int n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                factorial *= 1;
+            }
+
+            Console.WriteLine("El factorial de {0} es {1}", n, factorial);
+
+            Console.WriteLine("Los numeros enteros menores o iguales a {0} y divisibles entre 3 son: ", n);
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Los Numeros enteros menores o iguales a {0} y divisibles entre 5 son: ", n);
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Secuencia de numeros aumentandolos de 2  en dos hasta llegar a {0} es: ", n);
+
+            int j = 1;
+            while ( j <= n)
+            {
+                Console.WriteLine("{0} ", j);
+                j += 2;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("El factorial de {0} es {1}", n, factorial);
+        }
+    }
+}
---
+++ PAR/PROGRAMA 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        int N, suma = 0;
+
+        Console.Write("Por favor ingrese un numero entero positivo mayor que 0. ");
+        N = Convert.ToInt32(Console.ReadLine());
+
+        //utlizamos for para incrementar N y calcular la suma de los primeros N numeros enteros positivos.
+        for (int i = 1; i <= N; i++)
+        {
+            suma += i;
+        }
+
+        Console.WriteLine("Los numeros positivos menores o iguales a {0} son:", N);
+
+
+        for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine("{0} ", i);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Los numeros enteros impares menores o iguales que {0} son: ", N);
+
+        for (int i = 1; i <= N; i += 2)
+        {
+            Console.WriteLine("{0} ", i);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Tabla de multiplicar del numero {0}", N);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("{0} * {1} = {2}", N, i, N * i);
+
+            int j = N;
+            while (j <= 20)
+            {
+                Console.WriteLine("{0} ", j);
+                j += 2;
+            }
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("La suma de los primeros {0} numeros enteros positivos es: {1}", N, suma);
+
+        Console.WriteLine();
+    }
+}
